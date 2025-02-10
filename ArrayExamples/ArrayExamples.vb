f44a9ac80efa84b2d4c64999f2c7668401416592
Noah Holloway <noahholloway23@gmail.com>
2025-02-10 08:26:58 -0700

    add simple array examples

--- ArrayExamples/ArrayExamples.vb
+++ ArrayExamples/ArrayExamples.vb
@@ -1,6 +1,19 @@
-﻿Module ArrayExamples
+﻿'header
+Option Explicit On
+Option Strict On
+Option Compare Text
+Module ArrayExamples
 
     Sub Main()
+        'arrays are cool
+        Dim fruits(5) As String
+
+        fruits(0) = "banana"
+        fruits(3) = "grape"
+        'fruits(6) = "apple"
+
+        Console.WriteLine()
+
 
     End Sub
 
